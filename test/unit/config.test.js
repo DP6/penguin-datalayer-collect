f0403ae7259e15dd69715757560dfa0ac921e96e
Joaquim Neto <jsn.piata@gmail.com>
2021-09-06 20:20:09 -0300

    test: :white_check_mark: Adequação do teste do arquivo de configuração

--- test/unit/config.test.js
+++ test/unit/config.test.js
@@ -16,10 +16,6 @@ describe('Arquivo de configuração', () => {
   it('Deve possuir o atributo PARAM_QUERY_STRING_SCHEMA', async () => {
     expect(config).to.have.own.property('PARAM_QUERY_STRING_SCHEMA');
   });
-  it('Deve possuir o atributo BQ_DATASET_ID', async () => {
-    expect(config).to.have.own.property('BQ_DATASET_ID');
-    expect(config.BQ_DATASET_ID).to.equal('raft_suite');
-  });
   it('Deve possuir o atributo BQ_TABLE_ID_RAWDATA', async () => {
     expect(config).to.have.own.property('BQ_TABLE_ID_RAWDATA');
     expect(config.BQ_TABLE_ID_RAWDATA).to.equal('penguin_datalayer_raw');
